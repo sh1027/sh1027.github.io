2f90d3331630586b6083f9c15a2f01fd1a853b45
sh1027 <shiori.ueda@keio.jp>
2024-04-06 19:07:41 +0900

    Add robots.txt and sitemap.xml

---
+++ src/app/robots.ts
@@ -0,0 +1,12 @@
+import { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+    return {
+        rules: {
+            userAgent: '*',
+            allow: '/',
+            disallow: '/private/',
+        },
+        sitemap: 'https://sh1027.github.io//sitemap.xml',
+    }
+}
