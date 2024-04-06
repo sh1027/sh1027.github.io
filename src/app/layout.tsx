fe600f7bb6bbf9e5cfd703940f275259d09755f7
sh1027 <shiori.ueda@keio.jp>
2024-04-06 19:41:20 +0900

    Add icons to metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,14 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Favicon from '/public/favicon.ico'
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Shiori Ueda",
   description: "I am a Ph.D. student advised by Prof. Hideo Saito at Keio University, Japan. My research interests include computer vision and machine learning.",
+  icons: [{ rel: 'icon', url: Favicon.src }],
 };
 
 export default function RootLayout({
