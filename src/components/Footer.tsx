4e2f540eb4ad6459d6e9eab669df5a1f33084dec
sh1027 <shiori.ueda@keio.jp>
2024-04-10 01:31:03 +0900

    Add page break to Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,13 +7,14 @@ export default function Nav() {
   const profileData = profileJson.data;
   const today = new Date();
   return (
-    <footer className="bottom-0 z-10 w-full p-4 pt-10 flex flex-col justify-between bg-neutral-100 break-inside-avoid">
+    <footer className="bottom-0 z-10 w-full p-4 pt-10 flex flex-col justify-between bg-neutral-100 break-inside-avoid page-break-after">
       <span className="mx-auto text-sm text-center text-neutral-600">
         © {ccYear} {profileData.name}
       </span>
       <span className="mx-auto text-sm text-center text-neutral-600">
         Last updated: {today.toDateString()}
       </span>
+      <div className="page-break-before"></div>
     </footer>
   );
 }
